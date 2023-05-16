26f10e300e70b2f7bd9a730bd16849ebe8bf4e72
Quentin Watt <watt.quentin@gmail.com>
2023-05-16 11:21:10 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navigation from "./components/navigation/Navigation";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-200">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav className="w-full h-16 border-b bg-gray-800 text-white flex items-center px-3">
+      <span className="font-bold text-xl">Google maps point plotter</span>
+    </nav>
+  );
+};
+
+export default Navigation;
